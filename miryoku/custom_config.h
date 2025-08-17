cc0001f3ead56329d3c868e86d24a9b9ede8874b
t2hackdesigns <taishinglee@gmail.com>
2025-08-17 17:09:07 -0400

    custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -23,7 +23,7 @@ U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BT
 ) \
 &kp ESC   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp GRAVE \
 &kp LALT  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SEMI \
-&KP LSHFT K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp LSHFT \
+&kp LSHFT K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp LSHFT \
                     K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
